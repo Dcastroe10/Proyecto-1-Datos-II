e0f273e8318e6257ec760ebd142d047cfe7df337
Dcastroe10 <dcastroeli@gmail.com>
2022-03-25 20:40:08 -0600

    First Commit

---
+++ Proyecto-1-Datos-II-Daniel-C/client.cpp
@@ -0,0 +1,28 @@
+#include "client.h"
+#include "./ui_client.h"
+#include "iostream"
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
+
+using namespace std;
+
+Client::Client(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Client)
+{
+    ui->setupUi(this);
+}
+
+Client::~Client()
+{
+    delete ui;
+}
+
+
+void Client::on_Boton_prueba_clicked()
+{
+    QMessageBox::information(this,"Hola","prueba");
+
+}
+
---
+++ build-Proyecto-1-Datos-II-Daniel-C-Desktop_Qt_6_2_3_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,8 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Qt/App/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "PATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Qt/App/6.2.3/mingw_64/bin/qmake.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Qt/App/6.2.3/mingw_64" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Qt/App/Tools/mingw900_64/bin/gcc.exe" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Qt/App/Tools/mingw900_64/bin/g++.exe" CACHE "STRING" "" FORCE)
