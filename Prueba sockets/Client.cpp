1dc3117d91c9e6ff59c44a0bcdf28df898a9464f
Dcastroe10 <dcastroeli@gmail.com>
2022-03-28 19:07:22 -0600

    sockets error en el bind

--- Prueba sockets/Client.cpp
+++ Prueba sockets/Client.cpp
@@ -15,23 +15,27 @@ private:
     SOCKET servidor;
     sockaddr_in direccion_client_socket;
     char buffer [1024]; //size of message
+    std::string string_message;
 
     int wsa_start = WSAStartup(MAKEWORD(2,2), &wsaData);
 
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket = socket(AF_UNIX, SOCK_STREAM, 0); //AF_INET
 
 public:
     void create_client(){
         servidor = socket(AF_INET, SOCK_STREAM,0);
-        direccion_client_socket.sin_family = AF_INET;
+        direccion_client_socket.sin_family = AF_UNIX; //AF_INET;
         direccion_client_socket.sin_addr.s_addr = inet_addr("127.0.0.1");
-        direccion_client_socket.sin_port = htons(3030);
+        direccion_client_socket.sin_port = htons(8080);
+        std::cout <<wsa_start;
+        std::cout <<"ESTE ES EL WSAAAAAA";
     }
 
 
     void send_data(){  //cambiar el tipo de dato si quiero retornar algo!!!!!!!!
         std::cout <<"Que desea enviar el CLIENTE? ";
-        std::cin >> this->buffer;  //para ingresar el mensaje
+        //std::cin >> this->buffer;  //para ingresar el mensaje
+        std::cin >> this->string_message;
         std::cout << "\n";//std::cout << buffer << " CLIENTE, este es su mensaje???";
         send(servidor, buffer, sizeof(buffer), 0 );//revisar si aquí va server o client (del que se manda o al que se manda!!!
         //memset(buffer,0, sizeof(buffer));  //REINICIAR BUFFER CON LA ENTRADA!!!!
@@ -43,7 +47,7 @@ public:
         char mensaje;
         mensaje = recv(servidor,buffer, sizeof(buffer),0);//RECIBIR MENSAJE   AQUI ESTÁ ERROR NO ESTÁ RECIBIENDOOO
         std::cout <<"El SERVER dice: " << buffer<< " <---\n";
-        std::cout << mensaje<< " esto es el recieve";
+        std::cout << "-> " <<mensaje<< " <- esto es el recieve";
 
 
     }
--- Prueba sockets/Server.cpp
+++ Prueba sockets/Server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -8,44 +9,69 @@
 
 #include "Client.cpp"
 
+#define PORT_PORT 9090;
+
 #include <stdlib.h>
 #include <stdio.h>
 
 
-#pragma comment (lib, "Ws2_32")
+#pragma comment (lib, "Ws2_32.lib")
 
 
-
+#define PORT_PORT 1337
 class Server{
 private:
     WSAData wsaData;
-    int wsa_start = WSAStartup(MAKEWORD(2,2), &wsaData);
+    int wsa_start;
 
 
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
+    struct sockaddr_in server_addr;     //STRUCTS
+    struct sockaddr_in client_addr;
 
 
-    int server_socket = socket(AF_INET,SOCK_STREAM, 0);
+    std::string string_message;
 
 
-    SOCKET servidor = INVALID_SOCKET;
-    SOCKET cliente = INVALID_SOCKET;
+    int revisar_bind = 0;
+
+    int server_socket = INVALID_SOCKET;
+    int client_socket = 0; //CHANGE para poner varios maybeee o hacer otro
+
+
+    //SOCKET servidor = INVALID_SOCKET;
+    //SOCKET cliente = INVALID_SOCKET;
 
 
     char buffer [1024]; //size of messages
+    //creo que me lo volé por el string pero revisar mejor
 
 public:
-    void Start (){ //put always to start de server
-        server_address.sin_family=AF_INET;
-        server_address.sin_addr.s_addr = htons(INADDR_ANY);
-        server_address.sin_port = htons(3030);
-        bind(server_socket, (SOCKADDR *) & server_address, sizeof (server_address));
-        if (wsa_start != 0){
-            printf("Error del WSAStartup");
-        }else{
-            printf("sirvio el WSAStartup \n");
+    void Start () { //put always to start de server
+        wsa_start = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        server_socket =  socket(AF_INET,SOCK_STREAM, 0); //AF_UNIX
+
+        server_addr.sin_family = AF_UNIX;//AF_INET;
+        server_addr.sin_addr.s_addr = htons(INADDR_ANY);//inet_addr("127.0.0.1");//INADDR_ANY
+        server_addr.sin_port = htons(9090);
+        revisar_bind = bind(server_socket, (sockaddr *)&server_addr, sizeof(server_addr));
+        if (wsa_start != 0) {
+            printf("Error del WSASTARTUP");
+            WSACleanup();
+        } else {
+            if (server_socket == INVALID_SOCKET) {
+                std::cout << "no inicia el socket :((((((";
+            } else {
+                if(revisar_bind!=0){
+                    std::cout << "error del bind";
+                    std::cout << revisar_bind<< "  <----- ESTE ES EL BIND";
+                    closesocket(server_socket);
+                    std::cout<<WSAGetLastError()<<" <------ this is the get error";
+
+                }else{
+                printf("sirvio el incio!!! \n");
+            }
         }
+    }
     }
 
     int get_server_socket(){ //maybe delete revisar si sirve al final
@@ -54,11 +80,11 @@ public:
 
     int connect (){
         listen(server_socket, 3);
-        int size_client_addr = sizeof (client_address);
-        socklen_t client_lenght = sizeof(client_address);
+        int size_client_addr = sizeof (client_addr);
+        socklen_t client_lenght = sizeof(client_addr);
 
-        if ((cliente = accept(servidor, (struct sockaddr*)&client_address, reinterpret_cast<int *>(client_lenght))) != 0){
-            std::cout << "\n"; //se conectó
+        if ((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, reinterpret_cast<int *>(client_lenght))) != 0){
+            std::cout << "\n se conecto \n"; //
         }else{
             std::cout <<"error al conectarse lol :(";
         }
@@ -68,7 +94,7 @@ public:
 
     void recieve_data () { //cambiar el tipo de dato si quiero retornar algo!!!!!!!!
 
-        recv(cliente,buffer, sizeof(buffer),0); //RECIBIR MENSAJE
+        recv(client_socket,buffer, sizeof(buffer),0); //RECIBIR MENSAJE
         std::cout <<"El cliente dice: " << buffer;
         memset(buffer,0, sizeof(buffer));  //REINICIAR BUFFER CON LA ENTRADA!!!!
 
@@ -76,24 +102,28 @@ public:
 
     void send_data(){  //cambiar el tipo de dato si quiero retornar algo!!!!!!!!
         std::cout <<"QUE DESEA ENVIAR?? \n";
-        std::cin >> this->buffer;
-        std::cout << "\n"<<buffer << " este es su mensaje??? \n";
-        send(cliente, buffer, sizeof(buffer), 0 );//revisar si aquí va server o client (del que se manda o al que se manda
+        //std::cin >> this->buffer;
+        std::cin >> this->string_message;
+        std::cout << "\n"<<string_message << " este es su mensaje??? \n";
+        send(server_socket, buffer, sizeof(buffer), 0 );//revisar si aquí va server o client (del que se manda o al que se manda
         //memset(buffer,0, sizeof(buffer));  //REINICIAR BUFFER CON LA ENTRADA!!!!
         //std::cout << "el nuevo buffer es de: "<<buffer <<" no hay nada";
 
     }
 
     void close_all(){
-        closesocket(cliente);
-        closesocket(servidor);
+        closesocket(client_socket);
+        closesocket(server_socket);
         std::cout <<"se cerró todo";
     }
+
 };
 
 
 int main() {
     Server servidor;
+    //servidor.close_all();
+
     servidor.Start();
     //std::cout<<(Servidor.get_server_socket());
     //std::cout<<"Num de socket";
